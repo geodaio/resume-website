e43f8ba1ebee8a13c38ca6c43e225d77eb5d6196
geodaio <powheart26@proton.me>
2024-01-23 18:18:05 -0500

    Update head.php

--- api/php/head.php
+++ api/php/head.php
@@ -1,3 +1,21 @@
+<?php
+  if (strpos($_SERVER['REQUEST_URI'],'about')) {
+      $title = 'About';
+    }
+    else if (strpos($_SERVER['REQUEST_URI'],'resume')) {
+      $title = 'Resume';
+    }
+    else if (strpos($_SERVER['REQUEST_URI'],'work')) {
+      $title = 'Work';
+    }
+    else if (strpos($_SERVER['REQUEST_URI'],'contact')) {
+      $title = 'Contact';
+    }
+    else {
+      $title = 'Home';
+  }
+?>
+
 <head>
   <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">

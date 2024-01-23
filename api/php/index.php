c9fbd9961b10ebfbf84c9b8fb51d417190a2cac2
geodaio <powheart26@proton.me>
2024-01-23 18:11:29 -0500

    Update index.php

--- api/php/index.php
+++ api/php/index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
   <?php
-    if (strpos($_SERVER,'')) {
+    if (strpos($_SERVER['REQUEST_URI'],'about')) {
       $title = 'About';
     }
-    else if (strpos($_SERVER,'')) {
+    else if (strpos($_SERVER['REQUEST_URI'],'resume')) {
       $title = 'Resume';
     }
-    else if (strpos($_SERVER,'')) {
+    else if (strpos($_SERVER['REQUEST_URI'],'work')) {
       $title = 'Work';
     }
-    else if (strpos($_SERVER,'')) {
+    else if (strpos($_SERVER['REQUEST_URI'],'contact')) {
       $title = 'Contact';
     }
     else {

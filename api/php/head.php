c50b65e9361e5edb55bf75cb938a9ffbb3f0862c
geodaio <powheart26@proton.me>
2024-01-23 12:41:40 -0500

    Create head.php

---
+++ api/php/head.php
@@ -0,0 +1,15 @@
+<head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+
+    <title>
+        <?php
+          echo $title;
+        ?>
+    </title>
+
+  <link rel="shortcut icon" type="image/x-icon" href="/api/images/favicon.ico" id="favicon"/>
+    
+  <link rel="stylesheet" type="text/css" href="/api/css/style.php">
+</head>

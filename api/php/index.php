46cacab349ef43731c2d9153394819cf13ff6b12
geodaio <powheart26@proton.me>
2024-01-23 18:08:24 -0500

    Update index.php

--- api/php/index.php
+++ api/php/index.php
@@ -1,6 +1,22 @@
 <!DOCTYPE html>
 <html>
   <?php
+    if (strpos($_SERVER,'')) {
+      $title = 'About';
+    }
+    else if (strpos($_SERVER,'')) {
+      $title = 'Resume';
+    }
+    else if (strpos($_SERVER,'')) {
+      $title = 'Work';
+    }
+    else if (strpos($_SERVER,'')) {
+      $title = 'Contact';
+    }
+    else {
+      $title = 'Home';
+    }
+
     include 'head.php';
   ?>
   <body>

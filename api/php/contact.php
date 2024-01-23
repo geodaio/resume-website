ad1313bdb5c3275aacd681970d76a2e0a119d21c
geodaio <powheart26@proton.me>
2024-01-23 13:13:01 -0500

    Update contact.php

--- api/php/contact.php
+++ api/php/contact.php
@@ -1,1 +1,21 @@
-
+<p>Get in touch!</p>
+<form>
+  <label for="fname">First Name</label>
+  <input type="text" name="fname">
+  </br>
+  <label for="lname">Last Name</label>
+  <input type="text" name="lname">
+  </br>
+  <label for="email">Email</label>
+  <input type="email" name="email">
+  </br>
+  <label for="pnumber">Phone Number</label>
+  <input type="text" name="pnumber">
+  </br>
+  <label for="message">Message</label>
+  <input type="text" name="message">
+  </br>
+  <input type="submit" name="submit" value="Submit">
+</form>
+<p>Or, if you prefer, open your personal email provider.</p>
+<button id="open-email-prov">Contact me.</button>

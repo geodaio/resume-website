ae3edebe3c920f49adccb5a01e4a66a5cad0470f
geodaio <powheart26@proton.me>
2024-01-23 19:24:20 -0500

    Update resume.php

--- api/php/resume.php
+++ api/php/resume.php
@@ -7,7 +7,27 @@
     <?php
       include 'nav.php';
     ?>
-
-    <h1>Resume</h1>
+    <h1>Abigail Powell</h1>
+    <section>
+      <section>
+        <h2>Education</h2>
+      </section>
+      
+      <section>
+        <h2>Technical Skills</h2>
+      </section>
+      
+      <section>
+        <h2>Relevant Experience</h2>
+      </section>
+      
+      <section>
+        <h2>Project Experience</h2>
+      </section>
+      
+      <section>
+        <h2>Professional Work Experience</h2>
+      </section>
+    </section>
   </body>
 </html>

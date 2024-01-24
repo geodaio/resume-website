eb82a771facb7193a4e04bde0e959ff8f4d5251f
geodaio <powheart26@proton.me>
2024-01-23 19:04:40 -0500

    Update sendmessage.php

--- api/php/sendmessage.php
+++ api/php/sendmessage.php
@@ -1,3 +1,14 @@
 <?php
-  if($_POST[])
+  if(isset($_POST['contactform'])) {
+    if(count(array_filter($_POST)) != count($_POST)){
+        echo 'Please fill out all fields in the form!';
+    }
+    else {
+      $message = wordwrap($_POST['message'], 70);
+      $subject = $_POST[''];
+      $address = $_POST['email'];
+      mail('powheart26@proton.me', $subject, $message, 'From: ' . $address);
+      echo 'Your message has been sent. Please wait 2-3 business days for a reply.'; 
+    }
+  }
 ?>

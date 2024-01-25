4cb37c7e6c01da9a56842e6bd9f18e855db469a7
geodaio <powheart26@proton.me>
2024-01-25 13:47:32 -0500

    Update nav.php

--- api/php/nav.php
+++ api/php/nav.php
@@ -2,6 +2,6 @@
   <a href="api/php/aboutme.php" title="NULL" class="navLink">About</a>
   <a href="api/php/work.php" title="NULL" class="navLink">Work</a>
   <button><img src="logo.svg" id=navLogo"></button>
-  <a href="api/php/resume.php" title="NULL class="navLink"">Resume</a>
-  <a href="api/php/contact.php" title="NULL class="navLink"">Contact</a>
+  <a href="api/php/resume.php" title="NULL" class="navLink">Resume</a>
+  <a href="api/php/contact.php" title="NULL" class="navLink">Contact</a>
 </nav>

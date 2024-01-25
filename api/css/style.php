7fec674fe5f0cea95c1985c5dab1fc65fd381729
geodaio <powheart26@proton.me>
2024-01-25 13:48:40 -0500

    Update style.php

--- api/css/style.php
+++ api/css/style.php
@@ -2,9 +2,16 @@
   header('Content-Type: text/css');
 ?>
 
+/*Navigation*/
 nav {
   width: 100%;
   background-color: teal;
   text-align: center;
   padding: 2%;
+}
+.navLink{
+
+}
+#navlogo {
+
 }

c83dfeef583258928540d4fc68ed3c96bf1203e1
geodaio <powheart26@proton.me>
2024-01-25 13:45:38 -0500

    Update nav.php

--- api/php/nav.php
+++ api/php/nav.php
@@ -1,5 +1,4 @@
 <nav>
-  <a href="api/php/index.php" title="NULL">Home</a>
   <a href="api/php/aboutme.php" title="NULL">About</a>
   <a href="api/php/work.php" title="NULL">Work</a>
   <a href="api/php/resume.php" title="NULL">Resume</a>

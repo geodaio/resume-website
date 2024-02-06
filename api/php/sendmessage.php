3df0c512d6c9c029bb8282d9aa8e52e0bb6d1b1f
geodaio <powheart26@proton.me>
2024-02-06 14:57:29 -0500

    Update sendmessage.php

--- api/php/sendmessage.php
+++ api/php/sendmessage.php
@@ -1,5 +1,4 @@
 <?php
-    echo 'Mail Test successful';
     if(count(array_filter($_POST)) != count($_POST)){
         echo 'Please fill out all fields in the form!';
     }
@@ -7,11 +6,11 @@
       $message = wordwrap($_POST['message'], 70, '\r\n');
       $subject = $_POST['subject'];
       $address = $_POST['email'];
-      $headers = [
+      $headers = array(
         'From' => $address,
         'Reply-To' => $address,
         'X-Mailer' => 'PHP/' . phpversion()
-      ];
+      );
       
       if (mail('powheart26@proton.me', $subject, $message, $headers)){
         echo 'Your message has been sent. Please wait 2-3 business days for a reply.'; 

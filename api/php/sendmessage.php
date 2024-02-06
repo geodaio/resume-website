c2cbac5efbbf0bba0f50b2ae2d3a41b08e3f8e53
geodaio <powheart26@proton.me>
2024-02-06 14:47:24 -0500

    Update sendmessage.php

--- api/php/sendmessage.php
+++ api/php/sendmessage.php
@@ -4,13 +4,25 @@
         echo 'Please fill out all fields in the form!';
     }
     else {
-      $message = wordwrap($_POST['message'], 70);
+      $message = wordwrap($_POST['message'], 70, '\r\n');
       $subject = $_POST['subject'];
       $address = $_POST['email'];
+      $headers = [
+        'From' => $address,
+        'Reply-To' => $address,
+        'X-Mailer' => 'PHP/' . phpversion()
+      ];
         echo $subject;
         echo $message;
         echo $address;
-      mail('powheart26@proton.me', $subject, $message, 'From: ' . $address);
-      echo 'Your message has been sent. Please wait 2-3 business days for a reply.'; 
+      
+      $mailWorked = mail('powheart26@proton.me', $subject, $message, $headers);
+
+      if ($mailWorked == true){
+        echo 'Your message has been sent. Please wait 2-3 business days for a reply.'; 
+      }
+      else {
+        echo 'Error! Message send failure. Please try again.';
+      }
     }
 ?>
